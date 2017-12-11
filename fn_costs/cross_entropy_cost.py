601f9a516bc0809e33d6c4fa01f8a7a8441cbf45
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2017-12-11 20:00:18 +0100

    Add cross entropy fn derivative with explanation

--- fn_costs/cross_entropy_cost.py
+++ fn_costs/cross_entropy_cost.py
@@ -3,5 +3,36 @@ import numpy as np
 
 class CrossEntropyCost:
     @staticmethod
-    def fn(targets, predictions):
-        return np.sum(np.nan_to_num(-targets * np.log(predictions) - (1 - targets) * np.log(1 - predictions)))
+    def fn(y, a):
+        return np.sum(np.nan_to_num(-y * np.log(a) - (1 - y) * np.log(1 - a)))
+
+    @staticmethod
+    def fn_d(y, a):
+        return a - y
+
+
+"""
+    Cross entropy function derivative with respect to weights:
+
+     df     -y                          1 - y
+     --  =  --- * activation_fn'(z) - ------- * - activation_fn'(z) =
+     dw      a                          1 - a
+    
+             -y                         1 - y
+         =  --- * activation_fn'(z) + ------- * activation_fn'(z) = 
+             a                          1 - a
+    
+             -y     1 - y                      
+         = ( --- + ------- ) * activation_fn'(z) = 
+              a     1 - a
+    
+            -y + ya + a - ya                      
+         = ------------------ * activation_fn'(z) = 
+              a * (1 - a)
+    
+                  a - y                      
+         =  ------------------- * activation_fn'(z) = 
+             activation_fn'(z)
+    
+         =  a - y
+"""
